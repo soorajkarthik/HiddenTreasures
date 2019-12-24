938d2ce859a6e86c40ae1c859da75e573ec5b478
Sooraj Karthik <soorajkarthik15@gmail.com>
2019-12-24 17:25:45 -0600

    added map

--- app/src/main/java/com/example/hiddentreasures/MapFragment.java
+++ app/src/main/java/com/example/hiddentreasures/MapFragment.java
@@ -4,18 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.example.hiddentreasures.Model.User;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private FirebaseDatabase database;
     private DatabaseReference treasures;
     private String username;
     private User user;
     private View view;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,4 +38,28 @@ public class MapFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = view.findViewById(R.id.mapView);
+        if (mapView != null) {
+            mapView.onCreate(savedInstanceState);
+            mapView.getMapAsync(this);
+        }
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        MapsInitializer.initialize(getContext());
+
+        this.googleMap = googleMap;
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //googleMap.addMarker(new MarkerOptions().position(new LatLng(33.181128, -96.747210)).title("hi"));
+        //CameraPosition pos = CameraPosition.builder().target(new LatLng(33.181128, -96.747210)).build();
+        //googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(pos));
+    }
 }
