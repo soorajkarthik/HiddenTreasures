e2304392ba46ac56c9b7066e659f32ef2a2befeb
Sooraj Karthik <soorajkarthik15@gmail.com>
2020-01-04 21:52:21 -0600

    reformatted code according to google's style standards

--- app/src/androidTest/java/com/example/hiddentreasures/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/hiddentreasures/ExampleInstrumentedTest.java
@@ -1,12 +1,12 @@
 package com.example.hiddentreasures;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,11 +15,11 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.hiddentreasures", appContext.getPackageName());
-    }
+  @Test
+  public void useAppContext() {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+    assertEquals("com.example.hiddentreasures", appContext.getPackageName());
+  }
 }
