d8de2a3a3761f350adb0eb7978a33a84da556406
Sooraj Karthik <soorajkarthik15@gmail.com>
2020-01-09 08:23:31 -0600

    added documentation

--- app/src/main/java/com/example/hiddentreasures/NotificationService.java
+++ app/src/main/java/com/example/hiddentreasures/NotificationService.java
@@ -1,41 +1,30 @@
 package com.example.hiddentreasures;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import androidx.core.app.NotificationCompat;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class NotificationService extends FirebaseMessagingService {
 
+  /**
+   * Called when receives notification and app is in the foreground. Shows a toast rather than
+   * sending notification to notification tray. If app is in background/is completely closed, the
+   * user receives notifications that go to the notification tray as normal.
+   *
+   * @param remoteMessage The notification that was received
+   */
   @Override
   public void onMessageReceived(RemoteMessage remoteMessage) {
 
-    String messageBody = remoteMessage.getNotification().getBody();
-
-    Intent intent = new Intent(this, MainActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-    PendingIntent pendingIntent =
-        PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-    Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-
-    NotificationCompat.Builder notificationBuilder = new NotificationCompat
-        .Builder(getApplicationContext(), NotificationCompat.CATEGORY_SOCIAL)
-        .setSmallIcon(R.drawable.ic_smiley_face_white_24dp)
-        .setContentTitle("FCM Message")
-        .setContentText(messageBody)
-        .setAutoCancel(true)
-        .setSound(defaultSoundUri)
-        .setContentIntent(pendingIntent);
-
-    NotificationManager notificationManager =
-        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-    notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    Handler handler = new Handler(Looper.getMainLooper());
+    handler.post(() -> {
+      Toast.makeText(
+          getApplicationContext(),
+          remoteMessage.getNotification().getBody(),
+          Toast.LENGTH_LONG)
+          .show();
+    });
   }
 }
